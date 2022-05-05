18502c1ec75fdb56cb563e3a1d1dfc15a9ee4577
yvaldivia <yvaldivia@serv.externos.prismamp.com>
2022-05-05 15:50:30 -0300

    Testing Framework

--- src/test/java/runner/runner.java
+++ src/test/java/runner/runner.java
@@ -1,23 +1,14 @@
 package runner;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import pages.BasePage;
 
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features"},
-//        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true,
         glue = {"steps"},
         tags = "@regression"
