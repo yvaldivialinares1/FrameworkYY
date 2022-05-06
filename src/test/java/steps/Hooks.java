6b18dce27f539019e6e60c93543d965c43a73882
yvaldivia <yvaldivia@serv.externos.prismamp.com>
2022-05-06 17:21:39 -0300

    to delete chrome options

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -26,16 +26,9 @@ public class Hooks {
         "***********************************************************************************************************");
     WebDriverManager.chromedriver().setup();
     ChromeOptions options = new ChromeOptions();
-//    options.addArguments("--start-maximized");
-//    options.addArguments("--window-size=1920, 1080");
-//    options.addArguments("--disable-infobars");
-    options.addArguments("start-maximized"); // open Browser in maximized mode
-    options.addArguments("disable-infobars"); // disabling infobars
-    options.addArguments("--disable-extensions"); // disabling extensions
-    options.addArguments("--disable-gpu"); // applicable to windows os only
-    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-    options.addArguments("--no-sandbox"); // Bypass OS security model
-    
+    options.addArguments("--start-maximized");
+    options.addArguments("--window-size=1920, 1080");
+    options.addArguments("--disable-infobars");
     driver = new ChromeDriver(options);
     wait = new WebDriverWait(driver, 10);
     this.scenario = scenario;
